83daa7f1365dd522de267484b01ba72f818717c0
falconnine9 <63831264+falconnine9@users.noreply.github.com>
2022-05-28 17:14:12 -0600

    Fix MM placement

--- kernel/kernel.h
+++ kernel/kernel.h
@@ -3,8 +3,8 @@
 
 #define STACK_OFFSET  0x2200
 #define KERNEL_OFFSET 0x2210
-#define HEAP_OFFSET   0xA010
-#define HEAP_MAX      (HEAP_OFFSET + 0x8000000)
+#define HEAP_OFFSET   0x100000
+#define HEAP_MAX      0x8000000
 
 void k_init();
 
